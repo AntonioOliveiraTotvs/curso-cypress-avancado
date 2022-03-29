887d01f09dc0c24e48c4fce081ae590808caf36c
AntonioOliveiraTotvs <101591517+AntonioOliveiraTotvs@users.noreply.github.com>
2022-03-29 14:34:48 -0300

    Add files via upload

---
+++ cypress/support/commands.js
@@ -0,0 +1,10 @@
+import 'cypress-localstorage-commands'
+
+Cypress.Commands.add('assertLoadingIsShownAndHidden', () => {
+  cy.contains('Loading ...').should('be.visible')
+  cy.contains('Loading ...').should('not.exist')
+})
+
+Cypress.Commands.add('login', () => {
+  cy.visit('/')
+})
